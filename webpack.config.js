3d393994d45d5db034b2e7806a6324dcb99d6565
Alija Sabic <sabic.alija@gmail.com>
2019-04-05 09:39:55 +0200

    Test various webpack configurations

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,59 @@
 const { resolve } = require("path");
+const webpack = require("webpack");
+require("@babel/register");
 
 module.exports = {
   entry: "./src/index.js",
   output: {
     path: resolve(__dirname, "dist"),
-    filename: "index.js"
+    filename: "index.js",
+    library: "rest",
+    libraryTarget: "umd",
+    globalObject: `typeof self !== 'undefined' ? self : this`
   },
   module: {
     rules: [
       {
+        test: require.resolve("jquery"),
+        use: [
+          {
+            loader: "expose-loader",
+            options: "$"
+          }
+        ]
+      },
+      {
         test: /\.js$/,
-        use: "babel-loader",
-        exclude: /node_modules/
+        exclude: /(node_modules)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"]
+            // plugins: ["add-module-exports"]
+          }
+        }
       }
     ]
+  },
+  externals: {
+    jquery: {
+      commonjs: "jquery",
+      commonjs2: "jquery",
+      amd: "jquery",
+      root: "$"
+    }
   },
   stats: {
     colors: true
   },
   devtool: "source-map",
-  mode: process.env.NODE_ENV
+  mode: process.env.NODE_ENV,
+  optimization: {
+    usedExports: true
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      $: "jquery"
+    })
+  ]
 };
